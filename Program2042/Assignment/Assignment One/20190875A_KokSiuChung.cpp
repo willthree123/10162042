5af688278120c339df1543c11f8d7447af153e39
willthree123 <50272419+willthree123@users.noreply.github.com>
2021-03-11 11:53:08 +0800

    After1016 In-class tutorial

--- Program2042/Assignment/Assignment One/20190875A_KokSiuChung.cpp
+++ Program2042/Assignment/Assignment One/20190875A_KokSiuChung.cpp
@@ -10,31 +10,31 @@ using namespace std;
 
 void showInfo()
 {
-	// Fill in your personal particulars below
-	cout << "Name      : Kok Siu Chung\n";
-	cout << "Student ID: 20190875A\n";
-	cout << "Class     : 202B\n";
+    // Fill in your personal particulars below
+    cout << "Name      : Kok Siu Chung\n";
+    cout << "Student ID: 20190875A\n";
+    cout << "Class     : 202B\n";
 }
 
 void Q1() //Question 1
 {
     //Declare the necessary variables
     double inputX, inputY, centroidX, centroidY;
-    
-    //Set cout result in 2 decimal places and use "Fixed-point format"
-    cout<< setprecision(2);
-    cout<< fixed;
 
-	//Ask user to input the coordinates
-    cout <<"Input x: ";
+    //Set cout result in 2 decimal places and use "Fixed-point format"
+    cout << setprecision(2);
+    cout << fixed;
+
+    //Ask user to input the coordinates
+    cout << "Input x: ";
     cin >> inputX; //Input x
     cout << "Input y: ";
     cin >> inputY; //Input y
-    
+
     // Perform calculate
     //The three cooredinates are (0, 0) (inputX, 0) (inputX, inputY)
-    centroidX = ( (inputX+0+inputX)/3 );
-    centroidY = ( (inputY+0+0)/3 );
+    centroidX = ((inputX + 0 + inputX) / 3);
+    centroidY = ((inputY + 0 + 0) / 3);
 
     // Display the result with proper format
     cout << "Centroid is at (" << centroidX << "," << centroidY << ")";
@@ -45,33 +45,36 @@ void Q2() //Question 2
 {
     //Declare the necessary variables
     double a, b, c, ansOne, ansTwo;
-    
-    //Set cout result in 2 decimal places and use "Default format"
-    cout<< defaultfloat;
-    cout<< setprecision(6);
 
-	//Ask user to input the coordinates
-    cout <<"Enter a b c: ";
+    //Set cout result in 2 decimal places and use "Default format"
+    cout << defaultfloat;
+    cout << setprecision(6);
+
+    //Ask user to input
+    cout << "Enter a b c: ";
     cin >> a >> b >> c; //Input a b c
 
     //Calcuate if there is/are any solution
-    if( pow(b,2)-(4*a*c)>0 ){ //If there is/are any solution
+    if (pow(b, 2) - (4 * a * c) > 0)
+    { //If there is/are any solution
 
         //Calcuate Answer
-        ansOne = (-b-sqrt(pow(b,2)-(4*a*c)))/(2*a);
-        ansTwo = (-b+sqrt(pow(b,2)-(4*a*c)))/(2*a);
+        ansOne = (-b - sqrt(pow(b, 2) - (4 * a * c))) / (2 * a);
+        ansTwo = (-b + sqrt(pow(b, 2) - (4 * a * c))) / (2 * a);
         // Display the result with proper format
-        cout<<ansOne<<" "<<ansTwo;
+        cout << ansOne << " " << ansTwo;
     }
-    else if ( pow(b,2)-(4*a*c)==0 ) { //If there is only one solution
+    else if (pow(b, 2) - (4 * a * c) == 0)
+    { //If there is only one solution
         //Calcuate Answer
-        ansOne = (-b+sqrt(pow(b,2)-(4*a*c)))/(2*a);
+        ansOne = (-b + sqrt(pow(b, 2) - (4 * a * c))) / (2 * a);
         // Display the result with proper format
-        cout<<ansOne;
+        cout << ansOne;
     }
-    else { //There is/are no solution
+    else
+    { //There is/are no solution
         // Display the result with proper format
-        cout<<"No solution";
+        cout << "No solution";
     }
     // The end of question
 }
@@ -79,82 +82,102 @@ void Q2() //Question 2
 void Q3() //Question 3
 {
     //Declare the necessary variables
-    int Amount;
-    int day = 0;
-    
-	//Ask user to input the coordinates
-    cout <<"Starting money: $";
+    int Amount;     //Amount of input
+    int day = 0;    //day count
+    int result = 0; //result
+
+    //Ask user to input
+    cout << "Target money: $";
     cin >> Amount; //Input x
-    if(Amount==0){
-        cout << "Day " << day <<" you spend $" << day << " so you have $"<<Amount; // Display the result with proper format
-    }
-    while (Amount >0){//Run until using up all the money
-        Amount -= day; //Calcuate Answer
-        if(day!=0){
-            cout << "\n";
-        }
-        cout << "Day " << day <<" you spend $" << day << " so you have $"<<Amount; // Display the result with proper format
-        day++; //Next day
+
+    while (Amount > result)
+    {                                                                                //Run until saving up all the money
+        result = result + day;                                                       //Calcuate Answer
+        cout << "Day " << day << " you save $" << day << " so you have $" << result; // Display the result with proper format
+        day++;
+        if (Amount > result)
+        {
+            cout << endl;
+        } //Next day
     }
     // The end of question
 }
 
 void Q4() //Question 4
 {
-	//Declare the necessary variables
+    //Declare the necessary variables
     int size, firstNumber, tmpNum, tmpSize;
-    char symbol = '*';
-	//Ask user to input the coordinates
-    cout <<"Size: ";
+    char symbol = '#';
+    
+    //Ask user to input
+    cout << "Size: ";
     cin >> size; //Input size
     cout << "First number: ";
     cin >> firstNumber; //Input first number
+
     tmpSize = size;
-    for(int j=0; j < size; j++){
-        if(j!=0){
-            cout << "\n";        
+    for (int j = 0; j < size; j++)
+    {
+        if (j != 0)
+        {
+            cout << "\n";
         }
-        tmpNum=firstNumber;
-        for (int k=0; k<(size-tmpSize); k++){
+        tmpNum = firstNumber;
+        for (int i = 0; i < tmpSize - 1; i++) //print space
+        {
             cout << "  ";
         }
-        for (int i=0; i<tmpSize; i++){
+        for (int k = 0; k <= (size - tmpSize); k++) //print num and symbol
+        {
             cout << tmpNum << symbol;
-            tmpNum -- ;
+            tmpNum--;
         }
 
         tmpSize--;
     }
-    // The end of question 
+    // The end of question
 }
 
 // IMPORTANT: DO NOT MODIFY main()
 int main()
 {
-	char prog_choice;
+    char prog_choice;
 
-	do {
-		cout << "\n\n";
-		cout << "Program Selection Menu" << endl;
-		cout << "---------------------------------------" << endl;
-		cout << "Enter question number ('q' to quit): ";
-		cin >> prog_choice;
-		cout << "\n\n";
+    do
+    {
+        cout << "\n\n";
+        cout << "Program Selection Menu" << endl;
+        cout << "---------------------------------------" << endl;
+        cout << "Enter question number ('q' to quit): ";
+        cin >> prog_choice;
+        cout << "\n\n";
 
-		switch (prog_choice) {
-		case '0': showInfo(); break;
-		case '1': Q1(); break;
-		case '2': Q2(); break;
-		case '3': Q3(); break;
-		case '4': Q4(); break;
-		case 'q': break;
-		default:
-			cout << "No such question " << prog_choice << endl;
-			break;
-		}
-	} while (prog_choice != 'q');
+        switch (prog_choice)
+        {
+        case '0':
+            showInfo();
+            break;
+        case '1':
+            Q1();
+            break;
+        case '2':
+            Q2();
+            break;
+        case '3':
+            Q3();
+            break;
+        case '4':
+            Q4();
+            break;
+        case 'q':
+            break;
+        default:
+            cout << "No such question " << prog_choice << endl;
+            break;
+        }
+    } while (prog_choice != 'q');
 
-	cout << "Program terminates. Good bye!" << endl;
-	return 0;
+    cout << "Program terminates. Good bye!" << endl;
+    return 0;
 }
 // END
