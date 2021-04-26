62f73e7d710d9bc3aad53e7e48d9f3918ecb31b8
willthree123 <50272419+willthree123@users.noreply.github.com>
2021-04-26 20:25:42 +0800

    fixed Tutorial 10 Q1

--- Program2042/Tutorial Exercise/T10/T10_Q1.cpp
+++ Program2042/Tutorial Exercise/T10/T10_Q1.cpp
@@ -2,58 +2,37 @@
 #include <iomanip>
 using namespace std;
 
-int x = 1;
-int y[5] = {10, 20, 40, 50};
-
-int *xPtr = &x;
-int *yPtr = &y[0];
-
 int main()
 {
-    cout << "1a)" << endl;
-    cout << "Address of x: " << &x << endl;
-    cout << "Address of y: " << y << endl;
-    cout << endl;
-    cout << endl;
+    int x = 1;
+    int y[5] = {10, 20, 30, 40, 50};
 
-    cout << "1b)" << endl;
-    cout << "Value stored in xPtr: " << xPtr << endl;
-    cout << "Value stored in yPtr: " << yPtr << endl;
-    cout << endl;
-    cout << endl;
+    int *xPtr = &x;
+    int *yPtr = &y[0];
 
-    cout << "1c)" << endl;
-    cout << "'*' means that pointer and de-pointer" << endl;
-    cout << endl;
-    cout << endl;
+    cout << "Address of x = " << &x << endl;
+    cout << "Address of x = " << y << endl
+         << endl;
 
-    cout << "1d)" << endl;
-    cout << "(*yPtr + 2): " << (*yPtr + 2) << endl;
-    cout << "*(yPtr + 2): " << *(yPtr + 2) << endl;
-    cout << endl;
-    cout << endl;
+    cout << "xPtr = " << xPtr << endl;
+    cout << "yPtr = " << yPtr << endl
+         << endl;
 
-    cout << "1e)" << endl;
+    cout << "*xPtr = " << *xPtr << endl;
+    cout << "*yPtr = " << *yPtr << endl
+         << endl;
 
-    cout << "Before: " << endl;
-    cout << "xPtr:" << xPtr << "\t";
-    cout << "yPtr:" << yPtr << endl;
-    cout << "*xPtr:" << *xPtr << "\t";
-    cout << "*yPtr:" << *yPtr << endl;
+    cout << "*yPtr + 2 = " << *yPtr + 2 << endl;
+    cout << "*(yPtr + 2) = " << *(yPtr + 2) << endl
+         << endl;
+
     xPtr = yPtr + 2;
     (*xPtr)++;
     (*yPtr)++;
-
-    cout << endl;
-    cout << "After: " << endl;
-    cout << "xPtr:" << xPtr << "\t";
-    cout << "yPtr:" << yPtr << endl;
-    cout << "*xPtr:" << *xPtr << "\t";
-    cout << "*yPtr:" << *yPtr << endl
-         << endl;
-
-    cout << "value of y[2]:" << y[2] << "\t";
-    cout << "address of y[2]:" << &y[2] << "\t" << endl;
+    cout << "x = " << x << endl;
+    cout << "*xPtr = " << *xPtr << endl;
+    cout << "*yPtr = " << *yPtr << endl;
+    cout << "*(yPtr + 2) = " << *(yPtr + 2) << endl;
 
     return 0;
 }
